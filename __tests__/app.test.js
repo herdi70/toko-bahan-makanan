11795bd38b97c4cad53f65b53e239f40fde0018d
Herdi ansyah <email@example.com>
2026-05-12 21:13:18 +0800

    Final project files + CI ready

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,3 +1,13 @@
+// Mock browser APIs
+global.localStorage = {
+  getItem: jest.fn(() => null),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+};
+
+global.window = Object.create(global);
+window.location = { href: "" };
+
 const { sum } = require('../app');
 
 test('adds 1 + 2 to equal 3', () => {
